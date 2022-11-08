1acb11ed16fc060322bca4692e415852858d245d
Jek-San <ihsanzack@gmail.com>
2022-11-09 06:30:12 +0700

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Testimoni from './components/testimoni/Testimoni'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimoni />
+      <Contact/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/testimoni/Testimoni.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Testimoni.css'
+
+const Testimoni = () => {
+  return (
+    <div>Testimoni</div>
+  )
+}
+
+export default Testimoni
