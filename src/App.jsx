f36e0cbf4cba5dfd71520789acc9c7d87f9eb0a3
Jek-San <ihsanzack@gmail.com>
2022-11-09 06:52:43 +0700

    Done with general style m36

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 import Testimoni from './components/testimoni/Testimoni'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
@@ -18,7 +19,8 @@ const App = () => {
       <Services />
       <Portfolio />
       <Testimoni />
-      <Contact/>
+      <Contact />
+      <Footer/>
     </>
   )
 }
